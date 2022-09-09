d527b599c51d4b37650e32b064322efadff1cf23
Noah <noah.labrecue@gmail.com>
2022-09-09 12:07:53 -0400

    Stack tests

--- src/cpu/gbStack.rs
+++ src/cpu/gbStack.rs
@@ -21,17 +21,34 @@ impl GBStack for Cpu {
 	}
 
 	fn pop(&mut self) -> u16 {
-		let res = self.read_16(
-			ValueRefU16::Mem(
-				self.read_16(ValueRefU16::Reg(Register16::SP))
-			)
-		);
-		
 		self.write_16(
 			ValueRefU16::Reg(Register16::SP), 
 			self.read_16(ValueRefU16::Reg(Register16::SP))+2
 		);
-		
-		return res;
+		self.read_16(
+			ValueRefU16::Mem(
+				self.read_16(ValueRefU16::Reg(Register16::SP))
+			)
+		)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::cpu::{Cpu, values::ValueRefU16, registers::Register16};
+	use super::GBStack;
+
+	#[test]
+	fn stack_tests() {
+		let mut cpu = Cpu::new();
+		cpu.write_16(ValueRefU16::Reg(Register16::SP), 0xE000);
+		cpu.push(255);
+		assert_eq!(255, cpu.pop());
+		cpu.push(0);
+		cpu.push(1);
+		cpu.push(4);
+		assert_eq!(4, cpu.pop());
+		assert_eq!(1, cpu.pop());
+		assert_eq!(0, cpu.pop());
 	}
 }
