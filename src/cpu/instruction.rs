9e65d26fcca58e7f89dd0ccb5fa5cc6b96e1a907
Noah <noah.labrecue@gmail.com>
2022-09-13 13:13:47 -0400

    Whitespace removal

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -22,60 +22,39 @@ use super::{
 	values::{ValueRefU16, ValueRefI8, ValueRefU8}, 
 };
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 #[allow(non_camel_case_types)]
 pub enum Instruction {
 	NOP, 
 	STOP, 
 	ERROR(u8),
-
 	LD_8(ValueRefU8, ValueRefU8),
 	LDD_8(ValueRefU8, ValueRefU8),
 	LDI_8(ValueRefU8, ValueRefU8),
 	LD_16(ValueRefU16, ValueRefU16),
-
 	INC_8(ValueRefU8),
 	INC_16(ValueRefU16),
-
 	DEC_8(ValueRefU8),
 	DEC_16(ValueRefU16),
-
 	JR(Condition, ValueRefI8),
-
 	ADD_16(ValueRefU16, ValueRefU16),
-
 	ADD_SIGNED(ValueRefU16, ValueRefI8),
-	
 	ALU_OP_8(ALUOperation, ValueRefU8, ValueRefU8),
-
 	HALT,
-
 	CALL(Condition, ValueRefU16),
-
 	POP(CPURegister16),
 	PUSH(CPURegister16),
-
 	JP(Condition, ValueRefU16),
-
 	// Return
 	RET(Condition),
 	RETI,
-
 	RST(ValueRefU16),
-
 	DI,
 	EI,
 
 	// Accumulator flag ops
-
-	RLCA,
-	RRCA,
-	RLA,
-	RRA,
-	DAA,
-	CPL,
-	SCF,
-	CCF,
+	RLCA, RRCA, RLA, RRA,
+	DAA, CPL, SCF, CCF,
 
 	//  CB Instructions
 	BIT(u8, CPURegister8), 
@@ -95,6 +74,7 @@ pub enum Condition {
 pub enum ALUOperation {
 	ADD, ADC, SUB, SBC, AND, XOR, OR, CP
 }
+
 #[derive(Copy, Clone, Debug)]
 pub enum RotShiftOperation {
 	RLC, RRC, RL, RR, SLA, SRA, SWAP, SRL
