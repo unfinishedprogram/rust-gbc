90bf01b823d6bb42903c3a586c1c6b789f5574a8
Noah <noah.labrecue@gmail.com>
2022-09-13 13:05:00 -0400

    Added macros

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -4,12 +4,14 @@
 mod decode_tables;
 pub mod opcode;
 pub mod execute;
-
-
+pub mod mac_instruction;
 
 use opcode::Opcode;
 
 use crate::console_log;
+use crate::inst;
+use crate::arg;
+use crate::mem;
 use crate::log;
 
 use self::decode_tables::DT;
@@ -82,6 +84,8 @@ pub enum Instruction {
 	ROT(RotShiftOperation, CPURegister8)
 }
 
+use Instruction::*;
+
 #[derive(Copy, Clone, Debug)]
 pub enum Condition {
 	NZ, Z, NC, C, ALWAYS
@@ -97,115 +101,125 @@ pub enum RotShiftOperation {
 }
 
 pub fn get_instruction(cpu: &mut Cpu, opcode:Opcode) -> Instruction {
-	let x = opcode.x;
-	let z = opcode.z;
-	let y = opcode.y;
-	let p = opcode.p;
-	let q = opcode.q;
+	let x:usize = opcode.x as usize;
+	let z:usize = opcode.z as usize;
+	let y:usize = opcode.y as usize;
+	let p:usize = opcode.p as usize;
+	let q:usize = opcode.q as usize;
 
 	console_log!("{:#?}", opcode);
 
 	match (x, z, y, p, q) {
 	//(x, z, y, p, q)
-		(0, 0, 0, _, _) => Instruction::NOP,
-		(0, 0, 1, _, _) => Instruction::LD_16 (cpu.next_chomp().into(), SP.into()),
-		(0, 0, 2, _, _) => Instruction::STOP,
-		(0, 0, 3, _, _) => Instruction::JR(Condition::ALWAYS, (cpu.next_byte() as i8).into()),
-		(0, 0, _, _, _) => Instruction::JR(DT.cc[(y-4) as usize], (cpu.next_byte() as i8).into()),
+		(0, 0, 0, _, _) => inst!(cpu, NOP),
+		(0, 0, 1, _, _) => inst!(cpu, LD_16, SP, nn),
 
-		(0, 1, _, _, 0) => Instruction::LD_16 (DT.rp[p as usize].into(), cpu.next_chomp().into()),
-		(0, 1, _, _, 1) => Instruction::ADD_16(HL.into(), DT.rp[p as usize].into()),
+		(0, 0, 2, _, _) => inst!(cpu, STOP),
+		(0, 0, 3, _, _) => inst!(cpu, JR, (Condition::ALWAYS), d),
 
-		(0, 2, _, 0, 0) => Instruction::LD_8 (ValueRefU8::Mem(cpu.read_16(BC.into())), A.into()),
-		(0, 2, _, 1, 0) => Instruction::LD_8 (ValueRefU8::Mem(cpu.read_16(DE.into())), A.into()),
+		(0, 0, _, _, _) => inst!(cpu, JR, (DT.cc[(y-4)]), d),
+		(0, 1, _, _, 0) => inst!(cpu, LD_16, (DT.rp[p]), nn),
+
+		(0, 1, _, _, 1) => inst!(cpu, ADD_16, HL, (DT.rp[p])),
+
+		(0, 2, _, 0, 0) => inst!(cpu, LD_8, [BC]u8, A),
+		(0, 2, _, 1, 0) => inst!(cpu, LD_8, [DE]u8, A),
 
 		(0, 2, _, 2, 0) => {
-			let inst = Instruction::LD_8(ValueRefU8::Mem(cpu.read_16(HL.into())), A.into());
+			let inst = inst!(cpu, LD_8, [HL]u8, A);
 			cpu.write_16(HL.into(), cpu.read_16(HL.into())+1);
 			return inst;
 		},
 		
-		(0, 2, _, 3, 0) =>{
-			let inst = Instruction::LD_8(ValueRefU8::Mem(cpu.read_16(HL.into())), A.into());
+		(0, 2, _, 3, 0) => {
+			let inst = inst!(cpu, LD_8, [HL]u8, A);
 			cpu.write_16(HL.into(), cpu.read_16(HL.into())-1);
 			return inst;
 		},
-		(0, 2, _, 0, 1) => Instruction::LD_8 (A.into(), ValueRefU8::Mem(cpu.read_16(BC.into()))),
-		(0, 2, _, 1, 1) => Instruction::LD_8 (A.into(), ValueRefU8::Mem(cpu.read_16(DE.into()))),
-		(0, 2, _, 2, 1) => Instruction::LD_8 (A.into(), ValueRefU8::Mem(cpu.read_16(HL.into()))),
-		(0, 2, _, 3, 1) => Instruction::LD_8 (A.into(), ValueRefU8::Mem(cpu.read_16(HL.into()))),
 
-		(0, 3, _, _, 0) => Instruction::INC_16(DT.rp[p as usize].into()),
-		(0, 3, _, _, 1) => Instruction::DEC_16(DT.rp[p as usize].into()),
-		(0, 4, _, _, _) => Instruction::INC_8 (DT.r[y as usize].into()),
-		(0, 5, _, _, _) => Instruction::DEC_8 (DT.r[y as usize].into()),
-		(0, 6, _, _, _) => Instruction::LD_8  (DT.r[y as usize].into(), cpu.next_byte().into()),
+		(0, 2, _, 0, 1) => inst!(cpu, LD_8, A, [BC]u8),
+		(0, 2, _, 1, 1) => inst!(cpu, LD_8, A, [DE]u8),
+		(0, 2, _, 2, 1) => inst!(cpu, LD_8, A, [HL]u8),
+		(0, 2, _, 3, 1) => inst!(cpu, LD_8, A, [HL]u8),
 
-		(0, 7, 0, _, _) => Instruction::RLCA,
-		(0, 7, 1, _, _) => Instruction::RRCA,
-		(0, 7, 2, _, _) => Instruction::RLA,
-		(0, 7, 3, _, _) => Instruction::RRA,
-		(0, 7, 4, _, _) => Instruction::DAA,
-		(0, 7, 5, _, _) => Instruction::CPL,
-		(0, 7, 6, _, _) => Instruction::SCF,
-		(0, 7, 7, _, _) => Instruction::CCF,
+		(0, 3, _, _, 0) => inst!(cpu, INC_16, (DT.rp[p])),
+		(0, 3, _, _, 1) => inst!(cpu, DEC_16, (DT.rp[p])),
+		(0, 4, _, _, _) => inst!(cpu, INC_8, (DT.r[y])),
+		(0, 5, _, _, _) => inst!(cpu, DEC_8, (DT.r[y])),
+		(0, 6, _, _, _) => inst!(cpu, LD_8, (DT.r[y]), n),
 
-		(1, 6, 6, _, _) => Instruction::HALT,
-		(1, _, _, _, _) => Instruction::LD_8 (DT.r[y as usize].into(), DT.r[z as usize].into()),
+		(0, 7, 0, _, _) => inst!(cpu, RLCA),
+		(0, 7, 1, _, _) => inst!(cpu, RRCA),
+		(0, 7, 2, _, _) => inst!(cpu, RLA),
+		(0, 7, 3, _, _) => inst!(cpu, RRA),
+		(0, 7, 4, _, _) => inst!(cpu, DAA),
+		(0, 7, 5, _, _) => inst!(cpu, CPL),
+		(0, 7, 6, _, _) => inst!(cpu, SCF),
+		(0, 7, 7, _, _) => inst!(cpu, CCF),
 
-		(2, _, _, _, _) => Instruction::ALU_OP_8(DT.alu[y as usize], A.into(), DT.r[z as usize].into()),
+		(1, 6, 6, _, _) => inst!(cpu, HALT),
+		(1, _, _, _, _) => inst!(cpu, LD_8, (DT.r[y]), (DT.r[z])),
+
+		(2, _, _, _, _) => inst!(cpu, ALU_OP_8, (DT.alu[y]), A, (DT.r[z])),
 		
-		(3, 0, 0, _, _) => Instruction::RET(DT.cc[0]),
-		(3, 0, 1, _, _) => Instruction::RET(DT.cc[1]),
-		(3, 0, 2, _, _) => Instruction::RET(DT.cc[2]),
-		(3, 0, 3, _, _) => Instruction::RET(DT.cc[3]),
+		(3, 0, 0, _, _) => inst!(cpu, RET, (DT.cc[0])),
+		(3, 0, 1, _, _) => inst!(cpu, RET, (DT.cc[1])),
+		(3, 0, 2, _, _) => inst!(cpu, RET, (DT.cc[2])),
+		(3, 0, 3, _, _) => inst!(cpu, RET, (DT.cc[3])),
 
-		(3, 0, 4, _, _) => Instruction::LD_8(ValueRefU8::Mem(0xFF00 + cpu.next_byte() as u16), A.into()),
-		(3, 0, 5, _, _) => Instruction::ADD_SIGNED(SP.into(), ValueRefI8::Raw(cpu.next_displacement())),
-		(3, 0, 6, _, _) => Instruction::LD_8(A.into(), ValueRefU8::Mem(0xFF00 + cpu.next_byte() as u16)),
+		(3, 0, 4, _, _) => inst!(cpu, LD_8, [(0xFF00 + cpu.next_byte() as u16)]u8, A),
+
+		(3, 0, 5, _, _) => inst!(cpu, ADD_SIGNED, SP, d),
+
+		(3, 0, 6, _, _) => inst!(cpu, LD_8, A, [(0xFF00 + cpu.next_byte() as u16)]u8),
+
 		(3, 0, 7, _, _) => Instruction::LD_16(HL.into(), ValueRefU16::Raw(cpu.read_16(SP.into()).wrapping_add_signed(cpu.next_displacement() as i16))),
+		
+		(3, 1, _, _, 0) => inst!(cpu, POP, (DT.rp2[p])),
 
-		(3, 1, _, _, 0) => Instruction::POP(DT.rp2[opcode.p as usize]),
+		(3, 1, _, 0, 1) => inst!(cpu, RET, (Condition::ALWAYS)),
+		(3, 1, _, 1, 1) => inst!(cpu, RETI),
+		(3, 1, _, 2, 1) => inst!(cpu, JP, (Condition::ALWAYS), HL),
+		(3, 1, _, 3, 1) => inst!(cpu, LD_16, SP, HL),
 
-		(3, 1, _, 0, 1) => Instruction::RET(Condition::ALWAYS),
-		(3, 1, _, 1, 1) => Instruction::RETI,
-		(3, 1, _, 2, 1) => Instruction::JP(Condition::ALWAYS, HL.into()),
-		(3, 1, _, 3, 1) => Instruction::LD_16(SP.into(), HL.into()),
+		(3, 2, 0, _, _) => inst!(cpu, JP, (DT.cc[0]), nn),
+		(3, 2, 1, _, _) => inst!(cpu, JP, (DT.cc[1]), nn),
+		(3, 2, 2, _, _) => inst!(cpu, JP, (DT.cc[2]), nn),
+		(3, 2, 3, _, _) => inst!(cpu, JP, (DT.cc[3]), nn),
 
-		(3, 2, 0, _, _) => Instruction::JP(DT.cc[0], cpu.next_chomp().into()),
-		(3, 2, 1, _, _) => Instruction::JP(DT.cc[1], cpu.next_chomp().into()),
-		(3, 2, 2, _, _) => Instruction::JP(DT.cc[2], cpu.next_chomp().into()),
-		(3, 2, 3, _, _) => Instruction::JP(DT.cc[3], cpu.next_chomp().into()),
+		(3, 2, 4, _, _) => inst!(cpu, LD_8, [(0xFF00 + cpu.read_8(C.into()) as u16)]u8, A),
+		(3, 2, 5, _, _) => inst!(cpu, LD_8, [nn]u8, A),
 
-		(3, 2, 4, _, _) => Instruction::LD_8(ValueRefU8::Mem(0xFF00 + cpu.read_8(C.into()) as u16), A.into()),
-		(3, 2, 5, _, _) => Instruction::LD_8(ValueRefU8::Mem(cpu.next_chomp()), A.into()),
-		(3, 2, 6, _, _) => Instruction::LD_8(A.into(), ValueRefU8::Mem(0xFF00 + cpu.read_8(C.into()) as u16)),
-		(3, 2, 7, _, _) => Instruction::LD_8(A.into(), ValueRefU8::Mem(cpu.next_chomp())),
+		(3, 2, 6, _, _) => inst!(cpu, LD_8, A, [(0xFF00 + cpu.read_8(C.into()) as u16)]u8),
+		(3, 2, 7, _, _) => inst!(cpu, LD_8, A, [nn]u8),
 
-		(3, 3, 0, _, _) => Instruction::JP(Condition::ALWAYS, ValueRefU16::Raw(cpu.next_chomp())),
+
+		(3, 3, 0, _, _) => inst!(cpu, JP, (Condition::ALWAYS), nn),
+
 		(3, 3, 1, _, _) => {
 			let cb_opcode = Opcode::from(cpu.next_byte());
 			match cb_opcode.x {
-				0 => Instruction::ROT(DT.rot[cb_opcode.y as usize], DT.r[cb_opcode.z as usize]),
-				1 => Instruction::BIT(cb_opcode.y, DT.r[cb_opcode.z as usize]),
-				2 => Instruction::RES(cb_opcode.y, DT.r[cb_opcode.z as usize]),
-				3 => Instruction::SET(cb_opcode.y, DT.r[cb_opcode.z as usize]),
-				_ => Instruction::ERROR(cb_opcode.raw),
+				0 => inst!(cpu, ROT, (DT.rot[cb_opcode.y as usize]), (DT.r[cb_opcode.z as usize])),
+				1 => inst!(cpu, BIT, (cb_opcode.y), (DT.r[cb_opcode.z as usize])),
+				2 => inst!(cpu, RES, (cb_opcode.y), (DT.r[cb_opcode.z as usize])),
+				3 => inst!(cpu, SET, (cb_opcode.y), (DT.r[cb_opcode.z as usize])),
+				_ => inst!(cpu, ERROR, (cb_opcode.raw)),
 			}
 		},
-		(3, 3, 6, _, _) => Instruction::DI,
-		(3, 3, 7, _, _) => Instruction::EI,
+		
+		(3, 3, 6, _, _) => inst!(cpu, DI),
+		(3, 3, 7, _, _) => inst!(cpu, EI),
 
-		(3, 4, 0, _, _) => Instruction::CALL(DT.cc[0], cpu.next_chomp().into()),
-		(3, 4, 1, _, _) => Instruction::CALL(DT.cc[1], cpu.next_chomp().into()),
-		(3, 4, 2, _, _) => Instruction::CALL(DT.cc[2], cpu.next_chomp().into()),
-		(3, 4, 3, _, _) => Instruction::CALL(DT.cc[3], cpu.next_chomp().into()),
+		(3, 4, 0, _, _) => inst!(cpu, CALL, (DT.cc[0]), nn),
+		(3, 4, 1, _, _) => inst!(cpu, CALL, (DT.cc[1]), nn),
+		(3, 4, 2, _, _) => inst!(cpu, CALL, (DT.cc[2]), nn),
+		(3, 4, 3, _, _) => inst!(cpu, CALL, (DT.cc[3]), nn),
 
-		(3, 5, _, _, 0) => Instruction::PUSH(DT.rp2[p as usize]),
-		(3, 5, _, 0, 1) => Instruction::CALL(Condition::ALWAYS, cpu.next_chomp().into()),
+		(3, 5, _, _, 0) => inst!(cpu, PUSH, (DT.rp2[p])),
+		(3, 5, _, 0, 1) => inst!(cpu, CALL, (Condition::ALWAYS), nn),
 
-		(3, 6, _, _, _) => Instruction::ALU_OP_8(DT.alu[y as usize], A.into(), cpu.next_byte().into()),
-		(3, 7, _, _, _) => Instruction::RST(((opcode.y as u16) * 8).into()),
-		(_, _, _, _, _) => Instruction::ERROR(opcode.raw),
+		(3, 6, _, _, _) => inst!(cpu, ALU_OP_8, (DT.alu[y]), A, n),
+		(3, 7, _, _, _) => inst!(cpu, RST, ((opcode.y as u16) * 8)),
+		(_, _, _, _, _) => inst!(cpu, ERROR, (opcode.raw)),
 	}
 }
---
+++ src/cpu/instruction/mac_instruction.rs
@@ -0,0 +1,56 @@
+#[macro_export]
+macro_rules! arg {
+	($cpu:tt, d) => {$cpu.next_displacement().into()};
+	($cpu:tt, n) => {$cpu.next_byte().into()};
+	($cpu:tt, nn) => {$cpu.next_chomp().into()};
+	
+	($cpu:tt, $p:tt) => {$p.into()};
+}
+
+#[macro_export]
+macro_rules! mem {
+	($cpu:tt, [$p1:tt]u8) => {{
+		let v1 = arg!($cpu, $p1);
+		let v2 = $cpu.read_16(v1);
+		ValueRefU8::Mem(v2)
+	}};
+
+	($cpu:tt, [$p1:tt]u16) => {{
+		let v1 = arg!($cpu, $p1);
+		let v2 = $cpu.read_16(v1);
+		ValueRefU16::Mem(v2)
+	}};
+}
+
+#[macro_export]
+macro_rules! inst {
+	($cpu:ident, $inst:ident, $p1:tt, $p2:tt) => {
+		$inst(arg!($cpu, $p1), arg!($cpu, $p2))
+	};
+
+	($cpu:ident, $inst:ident, $p1:tt, $p2:tt, $p3:tt) => {
+		$inst(arg!($cpu, $p1), arg!($cpu, $p2), arg!($cpu, $p3))
+	};
+
+	($cpu:ident, $inst:ident, [$p1:tt]$t1:tt, [$p2:tt]$t2:ty) => {
+		$inst(mem!($cpu, [$p1]$t1), mem!($cpu, [$p2]$t2))
+	};
+
+	($cpu:ident, $inst:ident, [$p1:tt]$t:tt, $p2:tt) => {
+		$inst(mem!($cpu, [$p1]$t), arg!($cpu, $p2))
+	};
+
+	($cpu:ident, $inst:ident, $p1:tt, [$p2:tt]$t:tt) => {
+		$inst(arg!($cpu, $p1), mem!($cpu, [$p2]$t))
+	};
+
+	($cpu:ident, $inst:ident, $p1:tt) => {
+		$inst(arg!($cpu, $p1))
+	};
+
+	($cpu:ident, $inst:ident, [$p1:tt]$t:ty) => {
+		$inst(mem!($cpu, [$p1]$t))
+	};
+
+	($cpu:tt, $inst:tt) => { $inst };
+}
