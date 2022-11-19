257dbf1a71e2996b68f407c85c541f90db065044
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-19 08:33:20 -0500

    Small flag improvement

--- src/emulator/cpu/flags.rs
+++ src/emulator/cpu/flags.rs
@@ -3,38 +3,32 @@ use crate::emulator::state::EmulatorState;
 use super::{registers::CPURegister8, CPU};
 
 pub enum Flag {
-	Z = 7,
-	N = 6,
-	H = 5,
-	C = 4,
+	Z = 0b10000000,
+	N = 0b01000000,
+	H = 0b00100000,
+	C = 0b00010000,
 }
 
 pub trait Flags {
 	fn get_flag_byte(&self) -> u8;
 	fn set_flag_byte(&mut self, byte: u8);
 
-	fn set_flag(&mut self, flag: Flag) {
-		let mask = 1 << flag as usize;
-		let byte = self.get_flag_byte();
-		self.set_flag_byte(byte | mask);
-	}
-
 	fn set_flag_to(&mut self, flag: Flag, value: bool) {
-		match value {
-			true => self.set_flag(flag),
-			false => self.clear_flag(flag),
+		if value {
+			self.set_flag(flag)
+		} else {
+			self.clear_flag(flag)
 		}
 	}
 
 	fn clear_flag(&mut self, flag: Flag) {
-		let mask = 1 << flag as usize;
-		let byte = self.get_flag_byte();
-		self.set_flag_byte(byte & !mask);
+		self.set_flag_byte(self.get_flag_byte() & !(flag as u8));
 	}
-
+	fn set_flag(&mut self, flag: Flag) {
+		self.set_flag_byte(self.get_flag_byte() | flag as u8);
+	}
 	fn get_flag(&self, flag: Flag) -> bool {
-		let byte = self.get_flag_byte();
-		return (byte >> flag as usize) & 1 != 0;
+		self.get_flag_byte() & flag as u8 != 0
 	}
 }
 
