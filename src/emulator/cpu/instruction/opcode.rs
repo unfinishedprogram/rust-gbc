88da0cd7c7798e1b12f228e2c65a667c130bb095
Noah Labrecque <noah.labrecque@gmail.com>
2022-11-21 15:00:43 -0500

    micro optimization of opcode decoding

--- src/emulator/cpu/instruction/opcode.rs
+++ src/emulator/cpu/instruction/opcode.rs
@@ -1,10 +1,12 @@
 pub type Opcode = (usize, usize, usize, usize, usize);
 
 pub fn parse_opcode(raw: u8) -> Opcode {
-	let x = ((raw & 0b11000000) >> 6) as usize;
-	let y = ((raw & 0b00111000) >> 3) as usize;
-	let z = (raw & 0b00000111) as usize;
-	let p = ((raw & 0b00110000) >> 4) as usize;
-	let q = ((raw & 0b00001000) >> 3) as usize;
-	(x, z, y, p, q)
+	let raw = raw as usize;
+	(
+		((raw & 0b11000000) >> 6),
+		(raw & 0b00000111),
+		((raw & 0b00111000) >> 3),
+		((raw & 0b00110000) >> 4),
+		((raw & 0b00001000) >> 3),
+	)
 }
