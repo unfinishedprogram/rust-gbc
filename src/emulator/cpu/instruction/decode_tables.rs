1cef947cf2005733981e3d80cfcee3b7f2387f09
Noah Labrecque <noah.labrecque@gmail.com>
2022-12-12 12:06:41 -0500

    Reduced decode table code size

--- src/emulator/cpu/instruction/decode_tables.rs
+++ src/emulator/cpu/instruction/decode_tables.rs
@@ -1,6 +1,12 @@
 use super::{
-	ALUOperation, CPURegister16, CPURegister16::*, CPURegister8::*, Condition, RotShiftOperation,
-	ValueRefU16, ValueRefU8,
+	ALUOperation::{self, *},
+	CPURegister16,
+	CPURegister16::*,
+	CPURegister8::*,
+	Condition,
+	RotShiftOperation::{self, *},
+	ValueRefU16,
+	ValueRefU8::{self, *},
 };
 
 pub struct DecodeTables {
@@ -14,51 +20,23 @@ pub struct DecodeTables {
 
 pub const DT: DecodeTables = DecodeTables {
 	r: [
-		ValueRefU8::Reg(B),
-		ValueRefU8::Reg(C),
-		ValueRefU8::Reg(D),
-		ValueRefU8::Reg(E),
-		ValueRefU8::Reg(H),
-		ValueRefU8::Reg(L),
-		ValueRefU8::Mem(ValueRefU16::Reg(HL)),
-		ValueRefU8::Reg(A),
+		Reg(B),
+		Reg(C),
+		Reg(D),
+		Reg(E),
+		Reg(H),
+		Reg(L),
+		Mem(ValueRefU16::Reg(HL)),
+		Reg(A),
 	],
 
-	rp: [
-		CPURegister16::BC,
-		CPURegister16::DE,
-		CPURegister16::HL,
-		CPURegister16::SP,
-	],
+	rp: [BC, DE, HL, SP],
 
-	rp2: [
-		CPURegister16::BC,
-		CPURegister16::DE,
-		CPURegister16::HL,
-		CPURegister16::AF,
-	],
+	rp2: [BC, DE, HL, AF],
 
 	cc: [Condition::NZ, Condition::Z, Condition::NC, Condition::C],
 
-	alu: [
-		ALUOperation::ADD,
-		ALUOperation::ADC,
-		ALUOperation::SUB,
-		ALUOperation::SBC,
-		ALUOperation::AND,
-		ALUOperation::XOR,
-		ALUOperation::OR,
-		ALUOperation::CP,
-	],
+	alu: [ADD, ADC, SUB, SBC, AND, XOR, OR, CP],
 
-	rot: [
-		RotShiftOperation::RLC,
-		RotShiftOperation::RRC,
-		RotShiftOperation::RL,
-		RotShiftOperation::RR,
-		RotShiftOperation::SLA,
-		RotShiftOperation::SRA,
-		RotShiftOperation::SWAP,
-		RotShiftOperation::SRL,
-	],
+	rot: [RLC, RRC, RL, RR, SLA, SRA, SWAP, SRL],
 };
