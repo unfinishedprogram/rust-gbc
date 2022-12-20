7684ce4803e1736d005a82cb7dec8463aff782e7
Noah Labrecque <noah.labrecque@gmail.com>
2022-12-20 16:39:38 -0500

    Progress on MBC1

--- src/emulator/cartridge.rs
+++ src/emulator/cartridge.rs
@@ -1,4 +1,4 @@
-use log::{error, warn};
+use log::error;
 
 use self::header::{CartridgeInfo, CartridgeParseError, RawCartridgeHeader};
 
@@ -8,12 +8,20 @@ pub mod header;
 pub mod mbc;
 
 #[derive(Clone)]
+enum MBC1BankingMode {
+	Rom,
+	Ram,
+}
+#[derive(Clone)]
 pub struct CartridgeState {
 	pub info: CartridgeInfo,
 	pub raw_data: Vec<u8>,
-	pub selected_ram_bank: u16,
-	pub selected_rom_bank: u16,
+	pub bank_register: u8,
+
 	raw_ram: Vec<u8>,
+
+	banking_mode: MBC1BankingMode,
+	ram_enabled: bool,
 }
 
 impl CartridgeState {
@@ -21,13 +29,44 @@ impl CartridgeState {
 		log::error!("Len:{}", raw_data.len());
 		let raw_header = RawCartridgeHeader::from(&raw_data);
 		let info = raw_header.parse()?;
+
 		Ok(Self {
+			ram_enabled: false,
 			raw_ram: vec![0; (info.ram_banks * 0x2000) as usize],
 			info,
 			raw_data,
-			selected_ram_bank: 1,
-			selected_rom_bank: 1,
+			banking_mode: MBC1BankingMode::Rom,
+			bank_register: 1,
 		})
+	}
+
+	fn get_rom_bank(&self) -> u16 {
+		let bank_mask = match self.banking_mode {
+			MBC1BankingMode::Rom => 0b01111111,
+			MBC1BankingMode::Ram => 0b00011111,
+		};
+		let bank = self.bank_register & bank_mask;
+
+		(if bank & 0b00011111 == 0 { 1 } else { bank }) as u16 % self.info.rom_banks
+	}
+
+	fn get_ram_bank(&self) -> u16 {
+		(match self.banking_mode {
+			MBC1BankingMode::Rom => 0,
+			MBC1BankingMode::Ram => ((self.bank_register >> 5) & 0b11) % self.info.ram_banks as u8,
+		} as u16)
+	}
+
+	fn set_ram_bank(&mut self, value: u8) {
+		let value = value & 0b00000011;
+		self.bank_register &= 0b00011111;
+		self.bank_register |= value << 5;
+	}
+
+	fn set_rom_bank(&mut self, value: u8) {
+		let value = value & 0b00011111;
+		self.bank_register &= 0b01100000;
+		self.bank_register |= value;
 	}
 }
 
@@ -36,36 +75,26 @@ impl MemoryMapper for CartridgeState {
 		use mbc::MBC::*;
 
 		match self.info.mbc {
-			ROM => {
-				if matches!(addr, 0..0x8000) {
-					self.raw_data[addr as usize]
-				} else {
-					log::error!("Invalid Read memory: {addr}");
-					0
-				}
-			}
+			ROM => self.raw_data[addr as usize],
 
 			MBC1 => match addr {
 				0x0000..0x4000 => self.raw_data[addr as usize], // bank 0
 				0x4000..0x8000 => {
-					let raw_offset = 0x4000 * (self.selected_rom_bank.max(1) - 1);
-					self.raw_data[(addr + raw_offset) as usize]
+					let base = addr - 0x4000;
+					let raw_offset = 0x4000 * self.get_rom_bank();
+					self.raw_data[(base + raw_offset) as usize]
 				} // Bank X
 				0xA000..0xC000 => {
-					if self.info.ram_banks > 0 {
-						let mapped_addr = addr - 0xA000 + 0x2000 * self.selected_ram_bank;
-						if mapped_addr < self.info.ram_banks * 0x2000 {
-							self.raw_ram[mapped_addr as usize]
-						} else {
-							0
-						}
+					if self.info.ram_banks > 0 && self.ram_enabled {
+						let mapped_addr = 0x2000 * self.get_ram_bank() - 0xA000;
+						self.raw_ram[(addr + mapped_addr) as usize]
 					} else {
-						0
+						0xFF
 					}
 				}
 				_ => {
 					error!("MBC1 UnhandledRead: {:X}", addr);
-					0
+					0xFF
 				}
 			},
 			MBC2 => todo!(),
@@ -82,14 +111,31 @@ impl MemoryMapper for CartridgeState {
 	fn write(&mut self, addr: u16, value: u8) {
 		use mbc::MBC::*;
 		match self.info.mbc {
-			ROM => {
-				warn!("Write to readonly memory: {:X}", addr);
-			}
-			MBC1 => {
-				if let 0x2000..0x4000 = addr {
-					self.selected_rom_bank = (value & 0b00011111) as u16;
+			ROM => {}
+			MBC1 => match addr {
+				0x0000..0x2000 => self.ram_enabled = value & 0x0F == 0x0A,
+				0x2000..0x4000 => {
+					self.set_rom_bank(value);
 				}
-			}
+				0x4000..0x6000 => {
+					self.set_ram_bank(value);
+				}
+				0x6000..0x8000 => {
+					self.banking_mode = match value & 1 == 1 {
+						true => MBC1BankingMode::Ram,
+						false => MBC1BankingMode::Rom,
+					}
+				}
+				0xA000..0xC000 => {
+					if self.ram_enabled {
+						let mapped_addr = 0x2000 * self.get_ram_bank() - 0xA000;
+						self.raw_ram[(mapped_addr + addr) as usize] = value;
+					}
+				}
+				_ => {
+					log::error!("Writing outide of cartrage")
+				}
+			},
 			MBC2 => todo!(),
 			MMM01 => todo!(),
 			MBC3 => todo!(),
