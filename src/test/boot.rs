4fea72954855840adacebf617e876c3e7a419a63
Noah Labrecque <noah.labrecque@gmail.com>
2023-01-05 13:04:25 -0500

    Sped up tests

--- src/test/boot.rs
+++ src/test/boot.rs
@@ -1,6 +1,9 @@
-use crate::emulator::EmulatorState;
+use std::fs::read;
+
+use crate::emulator::{lcd::LCD, EmulatorState};
 extern crate test;
 
+use lazy_static::lazy_static;
 use test::Bencher;
 
 #[bench]
@@ -9,6 +12,20 @@ pub fn bench_boot(b: &mut Bencher) {
 
 	b.iter(|| {
 		state.step();
-		// state.run_until_boot();
 	})
+}
+
+lazy_static! {
+	pub static ref BOOTED_EMULATOR: EmulatorState = {
+		let mut state = EmulatorState::default();
+		// Not a specific rom, just one that has a valid logo and will pass checks
+		let rom = read("roms/test/dmg-acid2.gb").unwrap();
+		let lcd = LCD::new();
+
+		state.bind_lcd(lcd);
+		state.load_rom(&rom, "boot_rom".into()).unwrap();
+
+		state.run_until_boot();
+		state
+	};
 }
