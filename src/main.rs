2440680be4119a63775cbe57ca355bf96de9f9c7
Noah Labrecque <noah.labrecque@gmail.com>
2023-01-05 16:14:45 -0500

    ugly hosted rom loading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 #![feature(local_key_cell_methods)]
 
 use gbc_emu::application::{setup_listeners, APPLICATION};
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[allow(dead_code)]
+#[wasm_bindgen]
+pub fn load_rom(rom: &[u8], name: String) {
+	APPLICATION.with_borrow_mut(|app| {
+		app.load_rom(rom, name);
+		app.start();
+	});
+}
 
 fn main() {
 	APPLICATION.with_borrow_mut(|app| {
