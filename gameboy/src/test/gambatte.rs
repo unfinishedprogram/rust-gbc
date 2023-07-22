d9ea7414f72b566ca962177c946d442fb6631876
Noah Labrecque <noah.labrecque@gmail.com>
2023-07-22 11:00:06 -0400

    Massive speedup of gambatte tests

--- gameboy/src/test/gambatte.rs
+++ gameboy/src/test/gambatte.rs
@@ -77,8 +77,14 @@ fn gambatte(resource: &str) {
 fn get_test_output(test: &GambatteTest) -> String {
 	let mut state = init_emulator_with_rom_cgb(&test.path);
 
-	for _ in 0..1053360 / 4 {
+	let start = state.ppu.frame;
+
+	for _ in 0..1_053_360 / 4 {
 		state.step();
+
+		if state.ppu.frame - start >= 15 {
+			break;
+		}
 	}
 
 	let buffer = state.ppu.lcd.front_buffer();
@@ -91,19 +97,19 @@ fn screen_pixels(screen: &[u8]) -> Vec<Vec<bool>> {
 	for (y, pixel_row) in pixels.iter_mut().enumerate() {
 		for (x, pixel) in pixel_row.iter_mut().enumerate() {
 			let index = (x + y * 160) * 4;
+			*pixel = screen[index] == 0;
+			// let [r, g, b, a] = [
+			// 	screen[index],
+			// 	screen[index + 1],
+			// 	screen[index + 2],
+			// 	screen[index + 3],
+			// ];
 
-			let [r, g, b, a] = [
-				screen[index],
-				screen[index + 1],
-				screen[index + 2],
-				screen[index + 3],
-			];
-
-			*pixel = match (r, g, b, a) {
-				(0, 0, 0, 255) => true,
-				(255, 255, 255, 255) => false,
-				_ => unreachable!("Must be all black and white"),
-			};
+			// *pixel = match (r, g, b, a) {
+			// 	(0, 0, 0, 255) => true,
+			// 	(255, 255, 255, 255) => false,
+			// 	_ => unreachable!("Must be all black and white"),
+			// };
 		}
 	}
 	pixels
