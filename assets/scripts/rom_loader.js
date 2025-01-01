27ceb055d3d6669e98fe9ac7b15db8fe505c7c95
Noah Labrecque <noah.labrecque@gmail.com>
2025-01-01 14:23:03 -0500

    more roms in ui

--- assets/scripts/rom_loader.js
+++ assets/scripts/rom_loader.js
@@ -4,22 +4,30 @@ async function get_available_roms() {
   const res = await fetch("./roms.json");
   const json = await res.json();
   const dirs = json.dir.entries;
-  const games = dirs.filter(({ dir }) => dir.path == "roms/games")[0].dir
-    .entries;
-
-  console.log(games);
-  return games.map((game) => {
-    let name = game.file.path
-      .split("/")
-      .pop()
-      .replace(".gbc", "")
-      .replace(".gb", "");
-    let path = game.file.path;
-    return { name, path };
-  });
+  console.log(json);
+  return dirs;
 }
 
 function make_rom_button(rom) {
+  if ("dir" in rom) {
+    let dropdown = document.createElement("details");
+    let summary_elm = document.createElement("summary");
+    summary_elm.textContent = rom.dir.path;
+    dropdown.appendChild(summary_elm);
+
+    rom.dir.entries.forEach((entry) => {
+      dropdown.appendChild(make_rom_button(entry));
+    });
+    return dropdown;
+  } else if ("file" in rom) {
+    const elm = document.createElement("button");
+    let path = rom.file.path;
+    let name = path.split("/").pop().split(".")[0];
+    elm.onclick = () => load_rom({ name, path });
+    elm.innerText = name;
+    return elm;
+  }
+
   const elm = document.createElement("button");
   elm.onclick = () => load_rom(rom);
   elm.innerText = rom.name;
